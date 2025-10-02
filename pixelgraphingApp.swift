217eb746a3501c9d56f213ebd6c2ad8236a464e1
X-pert Menace <arianajansen14@gmail.com>
2025-10-02 17:55:09 +0100

    Add files via upload

---
+++ pixelgraphingApp.swift
@@ -0,0 +1,17 @@
+//
+//  pixelgraphingApp.swift
+//  pixelgraphing
+//
+//  Created by ariana jansen on 02/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct pixelgraphingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
